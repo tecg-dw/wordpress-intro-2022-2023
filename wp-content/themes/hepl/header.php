5e32aab9eb0768dcd3c5b5b93d06f0f761808980
Toon Van den Bos <toon@whitecube.be>
2023-03-03 16:12:43 +0100

    Added homepage animal section styling

--- wp-content/themes/hepl/header.php
+++ wp-content/themes/hepl/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= get_bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri() . '/public/css/site.css'; ?>" />
 </head>
 <body>
     <header class="header">
