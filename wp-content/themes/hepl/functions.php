1ee81ad8647088fe4ce78ff4422d99df45662ef4
Toon Van den Bos <toon@whitecube.be>
2023-03-24 17:27:35 +0100

    Added custom contact form (version 1)

--- wp-content/themes/hepl/functions.php
+++ wp-content/themes/hepl/functions.php
@@ -52,6 +52,15 @@ function hepl_register_custom_post_types()
         'menu_icon' => 'dashicons-pets', // https://developer.wordpress.org/resource/dashicons/#pets,
         'supports' => ['title','thumbnail'],
     ]);
+
+    register_post_type('message', [
+        'label' => 'Message de contact',
+        'description' => 'Les messages envoyés via le formulaire de contact.',
+        'public' => true,
+        'menu_position' => 20,
+        'menu_icon' => 'dashicons-email', // https://developer.wordpress.org/resource/dashicons/#pets,
+        'supports' => ['title','editor'],
+    ]);
 }
 
 add_action('init', 'hepl_register_custom_post_types');
@@ -76,3 +85,67 @@ function __hepl(string $translation, array $replacements = [])
     return $base;
 }
 
+// Gérer le formulaire de contact "custom"
+// Inspiré de : https://wordpress.stackexchange.com/questions/319043/how-to-handle-a-custom-form-in-wordpress-to-submit-to-another-page
+
+function hepl_validate_contact_form(array $data) : bool|array
+{
+    $errors = [];
+
+    if(! strlen($data['lastname'] ?? null)) {
+        $errors['lastname'] = 'Veuillez fournir votre nom de famille.';
+    }
+
+    if(! strlen($data['email'] ?? null)) {
+        $errors['email'] = 'Veuillez fournir votre adresse mail.';
+    } else if (! filter_var($data['email'] ?? null, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Votre adresse mail n\'est pas valide.';
+    }
+
+    return $errors ?: true;
+}
+
+function hepl_store_contact_form_message($firstname, $lastname, $email, $message)
+{
+    wp_insert_post([
+        'post_type' => 'message',
+        'post_status' => 'publish',
+        'post_title' => $firstname . ' ' . $lastname . ' <' . $email . '>',
+        'post_content' => $message,
+    ]);
+}
+
+function hepl_send_contact_form_message($firstname, $lastname, $email, $message)
+{
+    wp_mail('toon@whitecube.be', $firstname . ' ' . $lastname . ' <' . $email . '>', $message);
+}
+
+function hepl_execute_contact_form()
+{
+    $previous = wp_get_referer();
+
+    if(wp_verify_nonce($_POST['contact_nonce'] ?? null, 'hepl_contact_form') !== 1) {
+        // C'est pas bon, on ne continue pas l'exécution du script.
+        return;
+    }
+
+    if(is_array($errors = hepl_validate_contact_form($_POST))) {
+        // La validation ne passe pas, il faut afficher une erreur à l'utilisateur.
+        wp_safe_redirect($previous . '?' . http_build_query($errors));
+        exit;
+    }
+
+    $firstname = sanitize_text_field($_POST['firstname'] ?? '');
+    $lastname = sanitize_text_field($_POST['lastname'] ?? '');
+    $email = sanitize_email($_POST['email'] ?? '');
+    $message = sanitize_textarea_field($_POST['message'] ?? '');
+
+    hepl_store_contact_form_message($firstname, $lastname, $email, $message);
+    hepl_send_contact_form_message($firstname, $lastname, $email, $message);
+
+    wp_safe_redirect($previous);
+    exit;
+}
+
+add_action('admin_post_nopriv_hepl_contact_form', 'hepl_execute_contact_form');
+add_action('admin_post_hepl_contact_form', 'hepl_execute_contact_form');
--- wp-content/themes/hepl/template-about.php
+++ wp-content/themes/hepl/template-about.php
@@ -8,6 +8,45 @@
                 <?php the_content(); ?>
             </div>
         </div>
+        <div class="page__form" style="margin: 5em 0; padding: 5em 0; border-top: 1px solid black; border-bottom: 1px solid black;">
+            <form action="<?= esc_url(admin_url('admin-post.php')); ?>" method="POST" class="contact">
+                <fieldset class="contact__info">
+                    <div class="field">
+                        <label for="firstname" class="field__label">Votre prénom</label>
+                        <input type="text" name="firstname" id="firstname" class="field__input" />
+                        <?php if($_GET['firstname'] ?? null): ?>
+                            <p class="field__error" style="color:red"><?= $_GET['firstname']; ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="field">
+                        <label for="lastname" class="field__label">Votre nom</label>
+                        <input type="text" name="lastname" id="lastname" class="field__input" />
+                        <?php if($_GET['lastname'] ?? null): ?>
+                            <p class="field__error" style="color:red"><?= $_GET['lastname']; ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="field">
+                        <label for="email" class="field__label">Votre adresse mail</label>
+                        <input type="email" name="email" id="email" class="field__input" />
+                        <?php if($_GET['email'] ?? null): ?>
+                            <p class="field__error" style="color:red"><?= $_GET['email']; ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="field">
+                        <label for="message" class="field__label">Votre message</label>
+                        <textarea name="message" id="message" cols="30" rows="10" class="field__textarea"></textarea>
+                        <?php if($_GET['message'] ?? null): ?>
+                            <p class="field__error" style="color:red"><?= $_GET['message']; ?></p>
+                        <?php endif; ?>
+                    </div>
+                </fieldset>
+                <div class="contact__footer">
+                    <input type="hidden" name="action" value="hepl_contact_form" />
+                    <input type="hidden" name="contact_nonce" value="<?= wp_create_nonce('hepl_contact_form'); ?>" />
+                    <button class="contact__submit" type="submit">Envoyer</button>
+                </div>
+            </form>
+        </div>
         <div class="page__form">
             <?= apply_filters('the_content', '[contact-form-7 id="56" title="Formulaire page de contact"]'); ?>
         </div>
